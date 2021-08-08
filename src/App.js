08242abb2db77ceffd572ac36d7862ab89ca3c01
bushraaljafari <bushra.aljafari@gmail.com>
2021-08-08 17:21:59 +0300

    push part 1 frontend

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Header from './Header';
 import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
+///-----
+import { withAuth0 } from '@auth0/auth0-react';
+import BestBooks from './BestBooks';
+import Profile from './Profile';
+
+
+
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Login from './Login';
 
 class App extends React.Component {
 
   render() {
+    const {isAuthenticated}=this.props.auth0;
     console.log('app', this.props);
     return(
       <>
@@ -19,7 +28,14 @@ class App extends React.Component {
             <Header />
             <Switch>
               <Route exact path="/">
+                {isAuthenticated&&
+                <BestBooks/>}
+                {/* */}
+                <Login/>
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              </Route>
+              <Route exact path="/profile">
+                <Profile/>
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
             </Switch>
@@ -31,4 +47,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
