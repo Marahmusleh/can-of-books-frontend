af6a8c783621ed8108830c385b5509b3ef575485
marahmusleh <marahaltarefe18@gmail.com>
2021-08-10 20:28:20 +0300

    complete lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,86 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+import Carousel from 'react-bootstrap/Carousel';
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ownerEmail: this.props.auth0.user.email,
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchBooks();
+  }
+
+  fetchBooks = async () => {
+    try {
+      const responce = await axios.get(
+        `${process.env.REACT_APP_SERVER}/books?email=${this.state.ownerEmail}`
+      );
+      this.setState({
+        books: responce.data[0]?.books || [],
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
-    )
+    return (
+      <div>
+        <>
+          <Carousel>
+            {this.state.books.length > 0 &&
+              this.state.books.map((value) => (
+                <Carousel.Item>
+                  <img
+                    className='d-block w-30'
+                    style={{
+                      height: '500px',
+                      width: '700px',
+                      marginLeft: '29%',
+                    }}
+                    src={
+                      'https://t3.ftcdn.net/jpg/03/13/53/94/360_F_313539495_TIfAx53PwhMQopiuu7J1RiY2lVzSWrep.jpg'
+                    }
+                    alt='Book'
+                  />
+                  <Carousel.Caption>
+                    <h3
+                      style={{
+                        fontSize: '25px',
+                        backgroundColor: '#fff',
+                        color: '#333',
+                        width: '38%',
+                        textAlign: 'center',
+                        marginLeft: '30%',
+                      }}
+                    >
+                      {value.title}
+                    </h3>
+                    <p
+                      style={{
+                        fontSize: '18px',
+                        width: '34%',
+                        textAlign: 'center',
+                        marginLeft: '34%',
+                      }}
+                    >
+                      {value.description}
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+          </Carousel>{' '}
+        </>
+      </div>
+    );
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
